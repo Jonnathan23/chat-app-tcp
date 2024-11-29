3de0ac00603a9760011187147f20d8dd0370f67e
Jonnathan23 <jsaquicela26@gmail.com>
2024-11-29 12:43:22 -0500

    modularizacion de la aplicación

--- frontend2/src/main.js
+++ frontend2/src/main.js
@@ -1,78 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const { Socket } = require('net')
 const path = require('path');
-const {encrypt, decrypt} = require('./js/encryptation.js');
-
-let username = ''
-let socket
-
-/**
- * @description Contenedor de errores del socket
- */
-const SocketErrors = {
-    ECONNREFUSED: () => console.log('El servidor está inaccesible. Reintentando conexión...'),
-    ECONNRESET: () => console.log('El servidor cerró la conexión inesperadamente. Reintentando...'),
-    ETIMEDOUT: () => console.log('La conexión al servidor tardó demasiado. Reintentando...')
-}
-const SocketErrorDefualt = () => console.log('Error inesperado del socket')
-
-
-/**
- * @description Conectar al servidor
- */
-const connectToServer = () => {
-    try {
-        socket = new Socket();
-        socket.connect({ host: 'localhost', port: 8000 }, () => {
-            console.log('Conectado al servidor');
-            if (username) {
-                // Reenviar el nombre de usuario al reconectar
-                const encryptedUsername = encrypt(username);
-                socket.write(encryptedUsername);
-            }
-        });
-
-        // Evento 'ready' indica que el socket está completamente inicializado
-        socket.on('ready', () => console.log('Socket listo para enviar y recibir datos.'));
-
-        // Evento 'end' indica que el servidor cerró la conexión de forma ordenada
-        socket.on('end', () => console.log('Servidor solicitó cierre de conexión.'));
-
-        // Evento 'timeout' indica que el socket no ha recibido actividad en un tiempo determinado
-        socket.on('timeout', () => console.warn('El socket ha entrado en tiempo de espera (timeout).'));
-
-        // Evento data, Recibe un mensaje del servidor
-        socket.on('data', (data) => {
-            const decryptedMessage = decrypt(data.toString());
-            console.log('Mensaje del servidor:', decryptedMessage);
-
-            const mainWindow = BrowserWindow.getAllWindows()[0];
-            if (mainWindow) {
-                mainWindow.webContents.send('receive-message', decryptedMessage);
-            }
-        });
-
-        socket.on('error', (err) => {
-            console.error(` ----| | Error del socket | |---- `);
-            const error = SocketErrors[err.code] || SocketErrorDefualt
-            error()
-        });
-
-        socket.on('close', () => {
-            console.log('Conexión cerrada.');
-            setTimeout(() => {
-                console.log('Intentando reconectar...')
-                connectToServer()
-            }, 3000);
-        });
-
-
-
-    } catch (error) {
-        console.log('Error al conectar al servidor')
-        console.log(error)
-    }
-};
+const { connectToServer, sendMessage, setUsername } = require('./js/socketHandler.js');
 
 
 /**
@@ -96,21 +24,16 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     createWindow()
-    connectToServer();
-
-    ipcMain.on('send-message', (_, message) => {
-        if (socket && !socket.destroyed) {
-            const encryptedMessage = encrypt(message);
-            socket.write(encryptedMessage);
-        } else {
-            console.error('Socket no está conectado o está destruido');
+    connectToServer((message) => {
+        const mainWindow = BrowserWindow.getAllWindows()[0];
+        if (mainWindow) {
+            mainWindow.webContents.send('receive-message', message);
         }
-
     });
 
-    ipcMain.on('set-username', (_, user) => {
-        username = user; // Guardar el nombre de usuario
-        const encryptedUsername = encrypt(user);
-        socket.write(encryptedUsername);
-    });
+    // Enviar mensajes al servidor
+    ipcMain.on('send-message', (_, message) => sendMessage(message));
+
+    // Configurar el nombre de usuario
+    ipcMain.on('set-username', (_, user) => setUsername(user));
 })
