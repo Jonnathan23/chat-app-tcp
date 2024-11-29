5a4a2c4cebdd258b436f5c09584def70a3b11d7b
Jonnathan23 <jsaquicela26@gmail.com>
2024-11-29 01:56:54 -0500

    implementacion de login

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,27 +3,67 @@ const { Server } = require('net')
 const server = new Server()
 const port = 8000
 const host = '0.0.0.0'
-
 const END = 'END'
 
-server.on('connection', (socket) => {    
-    console.log(`Cliente conectado: ${socket.remoteAddress}:${socket.remotePort}`);
-    const {remotePort} = socket
-    socket.setEncoding('utf-8')
-    
-    socket.on('data', (message) => {
+const connections = new Map()
+
+/**
+ * @description Enviar un mensaje a todos los clientes excepto el origen
+ * @param {*} message --> mensaje de socket
+ * @param {*} origin  --> Socket origen
+ */
+const sendMessage = (message, origin) => {
+
+}
+
+server.on('connection', (socket) => {
+  console.log(`Cliente conectado: ${socket.remoteAddress}:${socket.remotePort}`);
+  const { remotePort } = socket
+  socket.setEncoding('utf-8')
+
+  connections.set(socket)
+
+  socket.on('data', (message) => {
+    /*
+      if (!connections.has(socket)) {
+        connections.set(socket, message)
+        socket.write(`Bienvenido ${message}`)
+        return
+      }
+  
       if (message === END) {
-        console.log(`Conexion ${remotePort} finalizada`)
         socket.end()
         return
       }
+      /*
+          for (const username of connections.values()) {
+            console.log(username)
+          }
+    
 
-      
-      console.log(`Mensaje recibido: ${message}`);
-      socket.write(`${remotePort} --> ${data}`)
-    })
+    const fullMessage = `${connections.get(socket)}:${message}`
+    socket.write(message)
+    //sendMessage(message, socket)
+    */
+
+    if (message === END) {
+      console.log(`Conexion ${remotePort} finalizada`)
+      socket.end()
+      return
+    }
+
+
+    console.log(`Mensaje recibido: ${message}`);
+    socket.write(`${remotePort} --> ${message}`)
+
+
+  })
+
+  socket.on('close', () => {
+    console.log(`Conexion ${remotePort} finalizada`)
+  })
 })
 
 server.listen(port, host, () => {
-    console.log(`Servidor TCP escuchando en el puerto ${port}`);
+  console.log(`Servidor TCP escuchando en el puerto ${port}`);
 })
---
+++ frontend2/src/js/preload.js
@@ -0,0 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+    sendMessage: (message) => ipcRenderer.send('send-message', message), // Enviar mensajes al main process
+    onMessage: (callback) => ipcRenderer.on('receive-message', (_, message) => callback(message)), // Escuchar mensajes
+});
